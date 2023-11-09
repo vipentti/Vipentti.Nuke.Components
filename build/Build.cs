5ebcbf6779cfb058e3b9345df7659acb99fc7dd8
Ville Penttinen <villem.penttinen@gmail.com>
2023-11-09 07:53:42 +0200

    Create github release after publish

--- build/Build.cs
+++ build/Build.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Components;
 using Vipentti.Nuke.Components;
@@ -30,8 +32,9 @@ namespace build;
     GitHubActionsImage.UbuntuLatest,
     GitHubActionsImage.MacOsLatest
     , OnPushBranches = new[] { MainBranch }
+    , EnableGitHubToken = true
 )]
-class Build : StandardNukeBuild, IUseCsharpier
+class Build : StandardNukeBuild, IUseCsharpier, ICreateGitHubRelease
 {
     public override string OriginalRepositoryName { get; } = "Vipentti.Nuke.Components";
     public override string MainReleaseBranch { get; } = MainBranch;
@@ -45,6 +48,15 @@ class Build : StandardNukeBuild, IUseCsharpier
     bool IUseCsharpier.UseGlobalTool { get; } = true;
 
     public override IEnumerable<Project> TestProjects { get; } = Enumerable.Empty<Project>();
+
+    string ICreateGitHubRelease.Name => From<IHazGitVersion>().Versioning.MajorMinorPatch;
+    IEnumerable<AbsolutePath> ICreateGitHubRelease.AssetFiles => From<IPack>().PackagesDirectory.GlobFiles("*.nupkg");
+    Target ICreateGitHubRelease.CreateGitHubRelease => _ => _
+        .Inherit<ICreateGitHubRelease>()
+        .TriggeredBy<IPublish>(x => x.Publish)
+        .ProceedAfterFailure()
+        .OnlyWhenDynamic(() => From<IPublishPackagesToNuGet>().ShouldPublishToNuGet)
+        .OnlyWhenStatic(() => From<IHazGitRepository>().GitRepository.IsOnMainOrMasterBranch());
 
     // Support plugins are available for:
     //   - JetBrains ReSharper        https://nuke.build/resharper
