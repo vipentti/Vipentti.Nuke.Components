b3603fe935866c44b25431ac1463bfcc1c151ce7
Ville Penttinen <villem.penttinen@gmail.com>
2024-02-18 11:17:49 +0000

    Enable formatters and local tools by default in StandardNukeBuild

--- src/Vipentti.Nuke.Components/StandardNukeBuild.cs
+++ src/Vipentti.Nuke.Components/StandardNukeBuild.cs
@@ -23,6 +23,7 @@ public abstract class StandardNukeBuild
     public abstract string MainReleaseBranch { get; }
     public abstract IEnumerable<Project> ProjectsToPack { get; }
     public abstract IEnumerable<IProvideLinter> Linters { get; }
+    public abstract IEnumerable<IProvideFormatter> Formatters { get; }
     public abstract IEnumerable<Project> TestProjects { get; }
     public abstract bool SignReleaseTags { get; }
 
@@ -54,34 +55,27 @@ public abstract class StandardNukeBuild
         var version = GitVersion.InformationalVersion;
 
         Serilog.Log.Information("BUILD SETUP");
-        Serilog
-            .Log
-            .Information(
-                "PackageOwner:   {Owner}",
-                From<IPublishPackagesToNuGet>().PackageOwner ?? "N/A"
-            );
-        Serilog
-            .Log
-            .Information(
-                "Configuration:  {Configuration}",
-                From<IHazConfiguration>().Configuration
-            );
+        Serilog.Log.Information(
+            "PackageOwner:   {Owner}",
+            From<IPublishPackagesToNuGet>().PackageOwner ?? "N/A"
+        );
+        Serilog.Log.Information(
+            "Configuration:  {Configuration}",
+            From<IHazConfiguration>().Configuration
+        );
         Serilog.Log.Information("Version:        {VersionSuffix}", version);
-        Serilog
-            .Log
-            .Information(
-                "TagVersion:     {TagVersion}",
-                From<IFinalizeRelease>().TagVersion ?? "(null)"
-            );
-        Serilog
-            .Log
-            .Information("CommitMessage:  {CommitMessage}", From<IFinalizeRelease>().CommitMessage);
-        Serilog
-            .Log
-            .Information(
-                "Publish:        {ShouldPublish}",
-                From<IPublishPackagesToNuGet>().ShouldPublishToNuGet
-            );
+        Serilog.Log.Information(
+            "TagVersion:     {TagVersion}",
+            From<IFinalizeRelease>().TagVersion ?? "(null)"
+        );
+        Serilog.Log.Information(
+            "CommitMessage:  {CommitMessage}",
+            From<IFinalizeRelease>().CommitMessage
+        );
+        Serilog.Log.Information(
+            "Publish:        {ShouldPublish}",
+            From<IPublishPackagesToNuGet>().ShouldPublishToNuGet
+        );
     }
 
     public T From<T>()
