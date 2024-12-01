03d987399135319cfadaa344a266f964ca8af43d
Ville Penttinen <villem.penttinen@gmail.com>
2024-12-01 13:16:43 +0200

    feat: Add support for SetupDotnetQuality

---
+++ src/Vipentti.Nuke.Components/DotnetVersionQuality.cs
@@ -0,0 +1,15 @@
+﻿// Copyright 2023 Ville Penttinen
+// Distributed under the MIT License.
+// https://github.com/vipentti/Vipentti.Nuke.Components/blob/main/LICENSE.md
+
+namespace Vipentti.Nuke.Components;
+
+public enum DotnetVersionQuality
+{
+    Default,
+    Daily,
+    Signed,
+    Validated,
+    Preview,
+    Ga,
+}
