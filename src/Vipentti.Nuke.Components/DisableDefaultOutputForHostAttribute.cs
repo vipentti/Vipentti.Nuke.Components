af5033408d6ac6e18037f26eb67dd696a982369b
Ville Penttinen <villem.penttinen@gmail.com>
2026-04-27 14:11:12 +0300

    feat: Upgrade Copyright year

--- src/Vipentti.Nuke.Components/DisableDefaultOutputForHostAttribute.cs
+++ src/Vipentti.Nuke.Components/DisableDefaultOutputForHostAttribute.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2023 Ville Penttinen
+﻿// Copyright 2023-2026 Ville Penttinen
 // Distributed under the MIT License.
 // https://github.com/vipentti/Vipentti.Nuke.Components/blob/main/LICENSE.md
 
